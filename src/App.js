a471fd0830d763deca25a5a2ad74fab56894a541
Jake O'Toole <otoolej33@gmail.com>
2020-07-23 10:25:35 -0500

    Add realm sdk. Created realmApp instance.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+// React
 import React from "react";
+// React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Realm
+import * as RealmWeb from "realm-web";
+
+// Components
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Users from "./routes/Users";
 import Navbar from "./components/Navbar";
+
+// Styling
 import "./App.css";
+
+// Realm App Initilization
+const realmApp = new RealmWeb.App({ id: process.env.REACT_APP_REALM_ID });
 
 function App() {
   return (
@@ -15,11 +26,11 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/home">
+            <Home />
           </Route>
           <Route path="/">
-            <Home />
+            <Users />
           </Route>
         </Switch>
       </div>
